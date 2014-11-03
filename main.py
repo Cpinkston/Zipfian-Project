5b2b9bfe18c6a6ee3ade02f3ec060842dea557e2
CPinkston <CPinksto@gmail.com>
2014-11-03 12:36:06 -0800

    Add function to get features of a sound

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def open_file(file):
     '''
     Input: String with the file location of a sound
 
-    Output: a list of values pertaining to the sound file
+    Output: A list of values pertaining to the sound file
 
     Reads in Wav files and extracts the numerical data to be used
     by within the file.
@@ -22,3 +22,37 @@ def open_file(file):
     trans_data =[(ele/2**8.)*2-1 for ele in data]
 
     return trans_data
+
+def get_features(file, sound_type = None, rank_type='mels'):
+    '''
+    Input: file: String with the file location of a sound,
+           sound_type: String with the label for the data,
+           rank_type: String 'mels' or 'log' to describe bucket type
+
+
+
+    Output: Dictionary containing all the bucket values and label for
+    for the data
+
+    Takes a file name and converts it to its feature row
+    
+    '''
+    #Start and finish for every bucket in mel's distribution
+    mels = [1,200,400,630,920,1270,1720,2320,3200,6400]
+    
+    sound = open_file(file):
+    sound_fft = fft(sound)
+    
+    bucket_scores={}
+
+    for i,x in enumerate(rank_type):
+        print i
+        if 1 == 0:
+            continue
+        else:
+            bucket_scores[rank_type['bucket'+str(i)]] = sum(sound_fft[x-1:x])
+
+    bucket_scores['type'] = sound_type
+
+    return bucket_scores
+
