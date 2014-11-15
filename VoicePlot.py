bcfcc7f8cc9e33a81056ca26943ae3fa14bf540e
CPinkston <CPinksto@gmail.com>
2014-11-15 14:33:14 -0800

    Add backgrounds for vowel sounds

--- VoicePlot.py
+++ VoicePlot.py
@@ -22,26 +22,62 @@ NUM_SAMPLES = 1024
 SAMPLING_RATE = 22050 #11025
 SPECTROGRAM_LENGTH = 50
 
-forest = pickle.load(open('/Users/CPinkston/Documents/Zipfian/FreedomOfSpeech/data/model.p'))
-
 #import model
-#bucket_names = ['bucket1','bucket2','bucket3','bucket4','bucket5','bucket6','bucket7','bucket8','label']
-#df = pd.read_csv("/Users/CPinkston/Documents/Zipfian/FreedomOfSpeech/data/data.csv",names=bucket_names)
-#y=df.pop('label').values
-#X=df.values
-#forest = RandomForestClassifier(n_estimators=100,criterion='entropy', max_features='log2', oob_score=True)
-#forest.fit(X,y)
+forest = pickle.load(open('/Users/CPinkston/Documents/Zipfian/FreedomOfSpeech/data/mono_model.p'))
 
 # Attributes to use for the plot view.
 size = (900,850)
 title = "Speech Visualizer"
 
 # Visual guide for sound
-background_dict = { 'Ae' : [50,0,0,0,0,0,0,0],\
-                    'Eh' : [0,50,0,0,0,0,0,0],\
-                    'Ee' : [0,0,50,0,0,0,0,0],\
-                    'Oh' : [0,0,0,50,0,0,0,0],\
-                    'Oo' : [0,0,0,0,50,0,0,0]}
+background_dict = {'AA':[ 35.81397578,  45.01631462,  56.12467144,  12.1484378 ,
+          3.06433121,   1.91775815,   3.08735659,   5.32401947,
+          5.88404492,   7.95661794,   1.88881055,   1.42798042,
+          3.96340981,   2.68320701,   1.9224381 ,   1.11476687],
+       'AE':[ 27.4362838 ,  27.10570391,  39.44961208,  12.29018975,
+         17.05977796,   7.38096405,  11.72786887,   5.76044961,
+          6.53821816,   4.01565196,   1.38825463,   2.09123181,
+          2.77621646,   1.32513547,   2.10078532,   1.66806562],
+       'AH':[ 37.75816345,  55.48377506,  27.80481956,  24.44436866,
+          3.1186638 ,   1.66242775,   5.13277218,   6.03904341,
+          9.05814786,   4.44443824,   1.02058099,   1.65270508,
+          2.93098263,   4.06752972,   1.6135137 ,   0.81619345],
+       'AO':[ 21.47497231,  38.28943443,  50.62903415,   8.35393119,
+          3.07678339,   2.05409526,   2.37562784,   2.00558598,
+          3.01447916,   4.26127112,   1.80530714,   3.73105018,
+          4.0233738 ,   2.07571379,   2.07032168,   1.29067984],
+       'EH':[ 26.22553365,  22.12785448,  12.18583196,   4.79216754,
+          9.64753079,   6.19215511,   7.10419455,   4.58124136,
+          2.65370963,   2.79506782,   0.82444456,   1.82331211,
+          1.80034007,   1.76603673,   1.79746801,   0.58000965],
+       'ER':[ 55.3946186 ,  56.88201025,  21.1990828 ,  20.20514299,
+          4.89875334,   1.44183263,   1.45997099,   1.14090613,
+          1.45707253,   0.98617129,   1.56760651,   2.98495273,
+          1.36142933,   1.09301032,   1.55692499,   1.14341959],
+       'EY':[ 39.19160161,  34.85463803,   4.64036407,   2.60255225,
+          2.95812851,   6.98613796,   4.91738817,   4.13260074,
+          1.85325952,   1.09616616,   1.35829042,   1.11305425,
+          1.5054683 ,   1.3815847 ,   1.64839403,   1.14386384],
+       'IH':[ 48.58006533,  54.00788813,   7.872923  ,   3.49289675,
+          4.49289906,  10.40966844,   7.84612386,   7.95802725,
+          5.29840227,   1.94301648,   1.99454648,   1.51040584,
+          2.0216913 ,   3.02829396,   3.92954506,   1.90158391],
+       'IY':[ 46.87647265,   9.43369867,   1.80606519,   1.05745142,
+          0.86969495,   1.49165165,   3.07262719,   2.78754269,
+          2.94247314,   1.10598322,   1.38521549,   1.90596473,
+          1.68691551,   3.4546833 ,   1.49868147,   0.90018443],
+       'OW':[ 27.38729711,  42.73110212,  19.68563542,   3.67982251,
+          2.04154194,   1.52837558,   2.75657802,   1.3685308 ,
+          2.38274303,   1.75837408,   1.22286706,   1.76399616,
+          1.77150212,   1.86287625,   1.39946777,   1.11275704],
+       'UH':[ 29.62068641,  33.75635882,   8.2471728 ,  10.92877852,
+          2.18208943,   1.00674882,   3.37916534,   3.20349283,
+          2.04546681,   5.33045881,   2.23010159,   1.43233783,
+          3.53001245,   1.31270093,   1.4417948 ,   0.62741355],
+       'UW':[ 29.84144362,  18.36840188,   6.89481727,   1.60053398,
+          0.98458783,   0.75937742,   0.83373466,   0.63936673,
+          0.67474447,   0.57706845,   0.62018357,   0.58136584,
+          0.74068797,   0.61835592,   0.84554567,   0.66147225]}
 
 def get_audio_data():
     pa = pyaudio.PyAudio()
@@ -53,7 +89,9 @@ def get_audio_data():
     sound_fft = abs(fft(normalized_data))[:NUM_SAMPLES/2]
     
     bucket_scores=[]
-    mels = array([1,200,400,630,920,1270,1720,2320,3200])/6.25
+    #mels = array([0,200,400,630,920,1270,1720,2320,3200])/6.25
+    mels = array([0,100,200,300,400,515,630,780,920,1095,1270,1495,1720,2020,2320,2769,3200])/6.25
+    
     
     plot_type = popup.plot_type
     
@@ -63,7 +101,7 @@ def get_audio_data():
         else:
             bucket_scores.append(sum(sound_fft[mels[i-1]:x]))
     classification = ['Spectrum']
-    if bucket_scores[0] > 10:
+    if bucket_scores[0] > 5:
         classification =  forest.predict(bucket_scores)
         
     return (array(bucket_scores), normalized_data, classification, plot_type)
@@ -71,22 +109,22 @@ def get_audio_data():
 def _create_plot_component(obj):
     # Setup the spectrum plot
     
-    frequencies = linspace(0.0, 8, num=8)
+    frequencies = linspace(0.0, 16, num=16) ##
     obj.spectrum_data = ArrayPlotData(frequency=frequencies)
-    empty_amplitude = zeros(8)
+    empty_amplitude = zeros(16)##
     obj.spectrum_data.set_data('amplitude', empty_amplitude)
-    background = array([3.5,.5,.25,.5,1,.5,.1,0])*20
+    background = array([50,0,0,0,0,0,0,0,50,0,0,0,0,0,0,0])*20
     obj.spectrum_data.set_data('background', background)
 
     obj.spectrum_plot = Plot(obj.spectrum_data)
     
-    obj.spectrum_plot.plot(("frequency", "background"), type='bar', bar_width=0.8, color='auto')
-    obj.spectrum_plot.plot(("frequency", "amplitude"), name="Spectrum", type='bar', bar_width=0.8, color='auto')#[0]
+    obj.spectrum_plot.plot(("frequency", "background"), value_scale="linear", type='bar', bar_width=0.8, color='auto')
+    obj.spectrum_plot.plot(("frequency", "amplitude"), value_scale="linear", name="Spectrum", type='bar', bar_width=0.8, color='auto')#[0]
     obj.spectrum_plot.padding = 50
     obj.spectrum_plot.title = "Spectrum"
     spec_range = obj.spectrum_plot.plots.values()[0][0].value_mapper.range
     spec_range.low = 0.0
-    spec_range.high = 100.0
+    spec_range.high = 50.0
     obj.spectrum_plot.index_axis.title = 'Frequency (hz)'
     obj.spectrum_plot.value_axis.title = 'Amplitude'
     
@@ -108,7 +146,6 @@ class TimerController(HasTraits):
         #spec_data = self.spectrogram_plot.values[1:] + [spectrum]
         #self.spectrogram_plot.values = spec_data
         self.spectrum_plot.request_redraw()
-        return
 
 
 class DemoHandler(Handler):
@@ -119,7 +156,6 @@ class DemoHandler(Handler):
         """
 
         info.object.timer.Stop()
-        return
     
 
 class Demo(HasTraits):
@@ -130,7 +166,8 @@ class Demo(HasTraits):
 
     timer = Instance(Timer)
     
-    plot_type = Enum("Ae", "Eh", "Ee", "Oh", "Oo")
+    plot_type = Enum("AA", "AE","AH", "AO", "EH", "ER", "EY", "IH", "IY", "OW", 
+    "UH", "UW")
 
     traits_view = View(
                     Group(
